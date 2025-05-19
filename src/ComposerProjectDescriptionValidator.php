5490937cec4d0dd35a4f654155c0b9fab152bce9
alanvdb <alanvdb.coder@gmail.com>
2025-05-19 06:07:38 +0200

    first code

---
+++ src/ComposerProjectDescriptionValidator.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace AlanVdb\Composer;
+
+use AlanVdb\Validator\Definition\ValidatorInterface;
+use AlanVdb\Validator\AbstractValidator;
+
+class ComposerProjectDescriptionValidator
+    extends AbstractValidator
+    implements ValidatorInterface
+{
+    
+    public function __construct()
+    {
+        parent::__construct('');
+    }
+
+    public function isValid(mixed $description): bool
+    {
+        if (!is_string($description)) {
+            $this->errorMessage = 'Project description must be a string';
+        }
+        if (empty($description)) {
+            $this->errorMessage = "Project description cannot be empty";
+        }
+        if (strlen($description) > 200) {
+            $this->errorMessage = 'Project description should be less than 200 characters';
+        }
+        if (!preg_match('/^[\w\s\-\.\,\!\?]+$/u', $description)) {
+            $this->errorMessage = 'Project description contains invalid characters';
+        }
+        return empty($this->errorMessage);
+    }
+}
---
+++ src/Factory/ComposerProjectDescriptionValidatorFactory.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace AlanVdb\Composer;
+
+use AlanVdb\Validator\Definition\ValidatorInterface;
+use AlanVdb\Composer\Definition\ComposerProjectDescriptionValidatorFactoryInterface;
+use AlanVdb\Composer\ComposerProjectDescriptionValidator;
+
+class ComposerProjectDescriptionValidatorFactory implements ComposerProjectDescriptionValidatorFactoryInterface
+{
+    public function create() : ValidatorInterface
+    {
+        return new ComposerProjectDescriptionValidator();
+    }
+}
